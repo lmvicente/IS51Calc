aadd074f7974ebff6e169f39f75f1bb7f2e2190a
lmvicente <leah.m.vicente@gmail.com>
2018-10-18 19:12:28 -0700

    error in ts file, need fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import * as angular from 'angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'IS51Calc';
 }
+
+const app = angular.module('myCalc', []);
+app.controller('calcCtrl', function ($scope) {
+
+});
+
